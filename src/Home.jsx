a6b6d8de5412b3f9cd806fedcde441c83b3bc13d
McFlink <mikaelcflink@gmail.com>
2025-01-26 09:23:52 +0100

    - Basic responsiveness, needs alot of work still

--- src/Home.jsx
+++ src/Home.jsx
@@ -47,43 +47,41 @@ const Home = () => {
           <div className="about">
             <h4>About me</h4>
             <p>
-              What does best describe me? Well, I LOVE coding, probably an
-              unhealthy amount of it. Why? There's always something new to
-              learn, and that suits my personality, and I'm not sorry I figured
-              that out after 17 years in construction, I'm just glad I did. The
-              reason I'm actually pretty good at it already is probably because
-              the love I feel about it.
+              Describe me? Well, I LOVE coding, probably an unhealthy amount of
+              it. Why? There's always something new to learn, and that suits my
+              personality, and I'm not sorry I figured that out after 17 years
+              in construction, I'm just glad I did.
             </p>
             <p>
               Well, enough with the boring stuff. I also love fishing, making
               youtube content about fishing, my kids, my fiance, and coding. I
               keep coming back to coding...
             </p>
-            <p>I love coding.</p>
             <p>Check out a selection of my projects to the right.</p>
-            <p>
-              <span>
-                <strong>Education:</strong> .NET Developer EXAM - 2025,
-                including
-              </span>
-            </p>
-            <ul>
-              <li>C#</li>
-              <li>Javascript</li>
-              <li>ASP.NET</li>
-              <li>Vue.js</li>
-              <li>Entity Framework</li>
-              <li>Blazor</li>
-              <li>React</li>
-              <li>HTML/CSS</li>
-              <li>Microservice archtecture</li>
-              <li>Cloud</li>
-              <li>Node.js</li>
-              <li>MSSQL</li>
-              <li>MongoDB</li>
-            </ul>
+            <div className="education-list">
+              <p>
+                <span>
+                  <strong>Education:</strong> .NET Developer EXAM - 2025
+                </span>
+              </p>
+              <ul>
+                <li>C#</li>
+                <li>Javascript</li>
+                <li>DOTNET, ASP.NET</li>
+                <li>Vue.js</li>
+                <li>Entity Framework</li>
+                <li>Blazor</li>
+                <li>React</li>
+                <li>HTML/CSS</li>
+                <li>Microservice archtecture</li>
+                <li>Cloud</li>
+                <li>Node.js</li>
+                <li>MSSQL</li>
+                <li>MongoDB</li>
+                <li>Docker</li>
+              </ul>
+            </div>
           </div>
-          <span className="profile-pic-borderbox"></span>
           <img className="profile-pic" src={profilePic} alt="image" />
         </div>
         <div className="right-box">
